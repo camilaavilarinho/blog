288b1d99b710ca164b8eb91614cc07f8826982d2
Camila Vilarinho <camilaavilarinho@gmail.com>
2020-03-21 16:27:09 -0300

    Update index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,44 @@
 import React from "react"
 import { Link } from "gatsby"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Button from "../components/button"
+import { theme } from "../theme/theme"
 
 class IndexPage extends React.Component {
   render() {
-    const siteTitle = "Gatsby Starter Personal Website"
+    const siteTitle = ""
 
     return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO
-          title="Home"
-          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
-        />
-        <img style={{ margin: 0 }} src="./GatsbyScene.svg" alt="Gatsby Scene" />
-        <h1>
-          Hey people{" "}
-          <span role="img" aria-label="wave emoji">
-            👋
-          </span>
-        </h1>
-        <p>Welcome to your new Gatsby website. You are on your home page.</p>
-        <p>
-          This starter comes out of the box with styled components and Gatsby's
-          default starter blog running on Netlify CMS.
-        </p>
-        <p>Now go build something great!</p>
-        <Link to="/blog/">
-          <Button marginTop="35px">Go to Blog</Button>
-        </Link>
-      </Layout>
+
+      <PageContainer>
+        <Layout location={this.props.location} title={siteTitle}>
+          <SEO
+            title="Home"
+            keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+          />
+          {/* <img style={{ margin: 0 }} src="./GatsbyScene.svg" alt="Gatsby Scene" /> */}
+          <Title>
+            Camila Vilarinho
+          </Title>
+          <Link to="/blog/">
+            <Button marginTop="35px">Go to Blog</Button>
+          </Link>
+        </Layout>
+      </PageContainer>
     )
   }
 }
+
+const PageContainer = styled.div`
+  background: ${theme.backgroundColor};
+`
+const Title = styled.h1`
+  font-size: 50px;
+  font-weight: 400;
+`
+
 
 export default IndexPage
