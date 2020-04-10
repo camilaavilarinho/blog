8b6577724ad515f73424585f0b5f92e8d0457c30
Camila Vilarinho <camilaavilarinho@gmail.com>
2020-04-10 15:32:01 -0300

    Update repo name and site url

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,20 +9,20 @@ module.exports = {
   siteMetadata: {
     title: `Camila Vilarinho`,
     description: `Meu blog pessoal`,
-    siteUrl: `https://delog-w3layouts.netlify.com/`,
+    siteUrl: `https://camiladev.netlify.com/`,
     home: {
       title: `Oi!`,
       description: `Estou em constante busca por aprender coisas novas, e às vezes sinto um chamado muito grande para compartilhar o que aprendo. 
-      Quando isso acontecer, irei reunir esses aprendizados aqui :)`
-    }
+      Quando isso acontecer, irei reunir esses aprendizados aqui :)`,
+    },
   },
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `markdown-pages`,
-        path: `${__dirname}/_data`
-      }
+        path: `${__dirname}/_data`,
+      },
     },
     {
       resolve: `gatsby-transformer-remark`,
@@ -35,25 +35,25 @@ module.exports = {
               inlineCodeMarker: null,
               aliases: {},
               showLineNumbers: false,
-              noInlineHighlight: false
-            }
+              noInlineHighlight: false,
+            },
           },
           {
-            resolve: "gatsby-remark-emojis"
-          }
-        ]
-      }
+            resolve: "gatsby-remark-emojis",
+          },
+        ],
+      },
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
         trackingId: "UA-30027142-1",
-        head: true
-      }
+        head: true,
+      },
     },
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-netlify-cms`
-  ]
-};
+    `gatsby-plugin-netlify-cms`,
+  ],
+}
